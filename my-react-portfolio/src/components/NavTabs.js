d478438a00045beef6452a811244c092fde0e526
Parker Scully <pscully05@gmail.com>
2023-03-03 11:35:17 -0500

    added navtab code, going to change based on what code goes in pages folder

--- my-react-portfolio/src/components/NavTabs.js
+++ my-react-portfolio/src/components/NavTabs.js
@@ -1,5 +1,45 @@
 import React from 'react';
 //import '../style'
-function NavTabs ({ currentPaghe, handlePageChange }) {
-    
+function NavTabs ({ currentPage, handlePageChange }) {
+    return (
+        <ul className="navbar">
+            <li className="navbar-item">
+                <a
+                    href="#portfolio"
+                    onClick={() => handlePageChange('Portfolio')}
+                    className={currentPage === 'Portfolio' ? 'active-nav' : 'nav-link'}
+                >
+                    Portfolio
+                </a>
+            </li>
+            <li className="navbar-item">
+                <a
+                    href="#resume"
+                    onClick={() => handlePageChange('Resume')}
+                    className={currentPage === 'Resume' ? 'active-nav' : 'nav-link'}
+                >
+                    Resume
+                </a>
+            </li>
+            <li className="navbar-item">
+                <a
+                    href="#aboutme"
+                    onClick={() => handlePageChange('Aboutme')}
+                    className={currentPage === 'Aboutme' ? 'active-nav' : 'nav-link'}
+                >
+                    Aboutme
+                </a>
+            </li>
+            <li className="navbar-item">
+                <a
+                    href="#contact"
+                    onClick={() => handlePageChange('Contact')}
+                    className={currentPage === 'Contact' ? 'active-nav' : 'nav-link'}
+                >
+                    Contact
+                </a>
+            </li>
+        </ul>
+    );
 }
+export default NavTabs;
